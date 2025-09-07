835a8f55349d5effd2f7d8b13ee25da7ec5353d4
Hediar <srlimvp@gmail.com>
2025-09-07 15:16:59 +0900

    string 반환 방식 + api, json형태 반환 방식

--- src/main/java/com/example/hello_spring/controller/HelloController.java
+++ src/main/java/com/example/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); // key, value으로 설정
         return "hello"; // resources > templates > hello.html 에서 렌더링
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // 데이터 그대로 반환
+    @GetMapping("hello-string")
+    @ResponseBody // 중요!!!, http에서의 body부분에 return 내용을 넣어주겠다.
+    public String helloString(@RequestParam(name = "name") String name) {
+        return "hello" + name;
+    }
+    // 객체 반환 (api 방식)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체 반환
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
